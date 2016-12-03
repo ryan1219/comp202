5428b1ac887a0335e686ee687da3f72ff6cb86bf
Yan Ren <ryan1219@live.com>
2016-12-02 23:36:54 -0500

    Image Reading done

--- src/fall2016/ImageFileUtilities.java
+++ src/fall2016/ImageFileUtilities.java
@@ -1,7 +1,92 @@
 package fall2016;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class ImageFileUtilities {
-	public static void read(String fileName){
+	public static Image read(String fileName) throws IOException{
+		String metadata = "";
+		int maxRange = 0;
+		String width = null, height = null;
+		Pixel[][] data = null;
+		Scanner sc = new Scanner(new File(fileName));
+		int totalPixelPoints = 0;
+		int counter = 0;
+		int i = 0;
+		int j = 0;
 		
+		String fileType = sc.next();
+		if(fileType.contains("P2")){
+			while(sc.hasNext()){
+				if(sc.hasNext("#")){
+					metadata = metadata + sc.nextLine();
+					System.out.println(metadata);
+				}else{
+					if(width == null){
+						width = sc.next();
+						System.out.println("width: "+width);
+					}else if(height == null){
+						height = sc.next();
+						System.out.println("height: "+height);
+						data = new Pixel[Integer.parseInt(height)][Integer.parseInt(width)];
+						totalPixelPoints = Integer.parseInt(height) * Integer.parseInt(width);
+					}else if(maxRange == 0){
+						maxRange = sc.nextInt();
+						System.out.println("maxRange: "+ maxRange);
+					}
+					else{
+						data[i][j] = new Pixel(sc.nextInt());
+//						System.out.println(sc.nextInt());
+						if(j < Integer.parseInt(width)-1){
+							j++;
+						}else{
+							j = 0;
+							i++;
+						}
+					}
+				}
+			}
+		}
+		else if(fileType.contains("P3")){
+			while(sc.hasNext()){
+				if(sc.hasNext("#")){
+					metadata = metadata + sc.nextLine();
+					System.out.println(metadata);
+				}else{
+					if(width == null){
+						width = sc.next();
+						System.out.println("width: "+width);
+					}else if(height == null){
+						height = sc.next();
+						System.out.println("height: "+height);
+						data = new Pixel[Integer.parseInt(height)][Integer.parseInt(width)];
+						totalPixelPoints = Integer.parseInt(height) * Integer.parseInt(width);
+					}else if(maxRange == 0){
+						maxRange = sc.nextInt();
+						System.out.println("maxRange: "+ maxRange);
+					}
+					else{
+						data[i][j] = new Pixel(sc.nextInt(), sc.nextInt(), sc.nextInt());
+//						System.out.println(sc.nextInt());
+						if(j < Integer.parseInt(width)-1){
+							j++;
+						}else{
+							j = 0;
+							i++;
+						}
+					}
+				}
+			}
+		}else{
+			throw new IOException("wrong file type");
+		}
+		
+		System.out.println(data[0][0].getRed());
+		System.out.println(data[Integer.parseInt(height)-1][Integer.parseInt(width)-1].getRed());
+		System.out.println(data[Integer.parseInt(height)-1][Integer.parseInt(width)-1].getGreen());
+		System.out.println(data[Integer.parseInt(height)-1][Integer.parseInt(width)-1].getBlue());
+		
+		return new Image(metadata, maxRange, data);
 	}
 }
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,20 +1,31 @@
 package test;
 
+import java.io.IOException;
+
 import fall2016.Image;
+import fall2016.ImageFileUtilities;
 import fall2016.Pixel;
 
 public class Test {
 
 	public static void main(String[] args) {
-		Pixel p1 = new Pixel(20, 100 ,20);
-	
-		Pixel[][] d = new Pixel[2][2];
+//		Pixel p1 = new Pixel(20, 100 ,20);
+//	
+//		Pixel[][] d = new Pixel[2][2];
+//		
+//		Image image = new Image("something for metadata", 100, d);
+//		
+//		image.getMetadata();
+//		
+//		System.out.println(p1.grey());
 		
-		Image image = new Image("something for metadata", 100, d);
-		
-		image.getMetadata();
-		
-		System.out.println(p1.grey());
+		try {
+//			ImageFileUtilities.read("C:\\Users\\Administrator\\Desktop\\catskypegrey.pgm");
+			ImageFileUtilities.read("C:\\Users\\Administrator\\Desktop\\catskype.pnm");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
